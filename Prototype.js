c59b4f3400a4ec4567b1f2fdf300efbe7d873829
Bikash Acharya <bikash.acharya614@gmail.com>
2024-10-16 11:35:02 +0545

    prototype

---
+++ Prototype.js
@@ -0,0 +1,21 @@
+let myUser = "Bikash   "
+
+console.log(myUser.trim().length);
+console.log(myUser.trueLength);
+
+let myArr = ["tori","horse"]
+
+let heroman = {
+    tori: "hammer",
+    horse: "black",
+
+    getSpiderPower : function(){
+    console.log(`Spidy power is ${this.horse}`);
+    }
+}
+Object.prototype.Bikash = function(){
+    console.log(`biku is present`);
+}
+
+// heroman.Bikash()
+myArr.Bikash()
